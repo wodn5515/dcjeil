7c6311c247df987056620cc1345e765b3caebb8a
wodn5515 <wodn5515@naver.com>
2019-12-21 17:24:26 +0900

    add deploy.py

--- dcjeil/settings/deploy.py
+++ dcjeil/settings/deploy.py
@@ -2,3 +2,16 @@ from .base import *
 
 
 DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
+        'HOST': os.environ['DB_HOST'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'NAME': os.environ['DB_NAME'],
+        'PORT': os.environ['DB_PORT']
+    }
+}
