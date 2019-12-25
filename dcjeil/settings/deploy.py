5e8ba0540479cbda91914281c4a74c8a51785a0a
wodn5515 <wodn5515@naver.com>
2019-12-25 17:13:14 +0900

    deploy DEBUG=True temp

--- dcjeil/settings/deploy.py
+++ dcjeil/settings/deploy.py
@@ -4,7 +4,7 @@ from dcjeil.util import get_server_info_value
 
 SETTING_PRD_DIC = get_server_info_value("production")
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = SETTING_PRD_DIC["SECRET_KEY"]
 
@@ -12,17 +12,4 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': SETTING_PRD_DIC['DATABASES']["default"]
-}
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "loggers": {
-        "django": {"handlers": ["console"], "level": "INFO"},
-    },
 }
