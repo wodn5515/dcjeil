106875977209d9b4f83cba497587fb8f841f909c
wodn5515 <wodn5515@naver.com>
2021-01-17 16:46:00 +0900

    Modify : util.py to utils.py

--- dcjeil/utils.py
+++ dcjeil/utils.py
@@ -11,12 +11,6 @@ def get_server_info_value(key: str):
                 return v
         raise ValueError('서버정보를 확인할 수 없습니다.')
 
-def get_filename(filename):
-    arr = [choice(string.ascii_letters) for _ in range(8)]
-    pid = ''.join(arr)
-    extension = filename.split('.')[-1]
-    return f'{pid}.{extension}'
-
 def get_social_login_secret_key(key: str):
 
     with open('social_login_secret_keys.json', mode='rt', encoding='utf-8') as file:
@@ -25,3 +19,4 @@ def get_social_login_secret_key(key: str):
             if k == key:
                 return v
         raise ValueError('서버정보를 확인할 수 없습니다.')
+
