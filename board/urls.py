d06867a839361c9d4263e8f344d398a87876a47e
wodn5515 <wodn5515@naver.com>
2021-01-17 16:48:57 +0900

    Modify : update post url

--- board/urls.py
+++ board/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('board/<str:pk>', views.Board.as_view(), name='board'),
     path('detail/<str:pk>', views.detail, name='detail'),
     path('write/<str:board_pk>', views.post_write, name="write"),
-    path('update/<str:board_pk>/<str:pk>', views.post_update, name="update"),
+    path('update/<str:pk>', views.post_update, name="update"),
     path('delete/<str:pk>', views.post_delete, name="delete")
 ]
