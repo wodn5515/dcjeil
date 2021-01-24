9f9071b5218ed09d2fb01723f2623f54e5d4b72e
wodn5515 <wodn5515@naver.com>
2021-01-24 15:42:42 +0900

    Fix : comment writer error

--- data/serializers.py
+++ data/serializers.py
@@ -5,7 +5,7 @@ class CommentSerializer(serializers.ModelSerializer):
     writer = serializers.SerializerMethodField()
 
     def get_writer(self, obj):
-        return "관리자" if obj.writer.is_superuser else obj.writer
+        return "관리자" if obj.writer.is_superuser else obj.writer.name
         
     class Meta:
         model = Comment
